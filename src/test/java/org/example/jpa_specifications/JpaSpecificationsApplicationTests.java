bf3d0250e9a2b77b3116f819f1a4622231f49743
Terekhov <aterekhov@digital-spirit.ru>
2025-06-27 17:46:04 +0400

    init commit

---
+++ src/test/java/org/example/jpa_specifications/JpaSpecificationsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.jpa_specifications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaSpecificationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
